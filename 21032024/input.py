08106e051de302e91e4252e8a31d0265f1818f4d
RajKKapadia <raajforyou@gmail.com>
2024-03-21 22:18:25 +0530

    added input

---
+++ 21032024/input.py
@@ -0,0 +1,11 @@
+name = input('Enter your name -> ')
+
+print(type(name))
+
+print(f'Your name is {name}.')
+
+age = int(input('Enter your age -> '))
+
+print(type(age))
+
+print(f'Your age is {age} years.')
