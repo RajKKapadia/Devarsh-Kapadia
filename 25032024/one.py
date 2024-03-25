f6e8f44b1e02b9fe52df6bb08d4b7f62a566d922
RajKKapadia <raajforyou@gmail.com>
2024-03-25 17:09:58 +0530

    added loop patern problems

--- 25032024/one.py
+++ 25032024/one.py
@@ -12,16 +12,93 @@
 0 1 2 3
 0 1 2 3 4
 '''
-n = int(input('Enter a number - '))
+# n = int(input('Enter a number - '))
 
-for i in range(1, n+1):
-    for j in range(1, i+1):
-        print(j, end=' ')
-    print('')
+# for i in range(1, n+1):
+#     for j in range(1, i+1):
+#         print(j, end=' ')
+#     print('')
 
 # n = str(input('Enter your name - '))
 
 # for i in range(len(n)):
-#     for j in range(i):
-#         print(n[j], end=' ')
+#     for j in range(i+1):
+#         print(n[j], end='')
 #     print('')
+
+'''
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+'''
+
+# for i in range(0, 10, 2):
+#     print(i)
+
+# for i in range(10, 0, -1):
+#     print(i)
+
+# Problem - 1
+'''
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+'''
+
+# Problem 2
+'''
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+'''
+
+# Problem - 3
+'''
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+'''
+
+# Problem - 4
+'''
+1 1 1 1 1
+2 2 2 2
+3 3 3
+4 4
+5
+'''
+
+# Problem - 5
+'''
+5 5 5 5 5
+5 5 5 5
+5 5 5
+5 5
+5
+'''
+
+# Problem - 6
+'''
+5 4 3 2 1
+5 4 3 2
+5 4 3
+5 4
+5
+'''
+
+# Problem - 7
+'''
+1 2 3 4 5
+2 2 3 4 5
+3 3 3 4 5
+4 4 4 4 5
+5 5 5 5 5
+'''
