735062a2f0f012f10cd1cee373884d001784b3a8
RajKKapadia <raajforyou@gmail.com>
2024-03-27 21:33:08 +0530

    more example on factors

---
+++ 27032024/one.py
@@ -0,0 +1,63 @@
+'''Python Program to Check if a Number is Positive, Negative or 0
+'''
+
+'''Loop
+If Else statements
+'''
+
+# n = int(input('Enter a number - '))
+
+# if n > 0:
+#     print(f'{n} is positive.')
+# elif n == 0:
+#     print(f'{n} is zero.')
+# else:
+#     print(f'{n} is negative.')
+
+'''Python Program to Find the Factorial of a Number
+5 factorial
+5 facorital = 1 * 2 * 3 * 4 * 5
+'''
+
+# factorial *= i
+# factorial = factorial * i
+
+# n = int(input('Enter a number - '))
+# factorial = 1
+
+# for i in range(2, n + 1):
+#     factorial *= i
+
+# print(factorial)
+
+'''Python Program to Find the Factors of a Number
+'''
+# 2   24
+# 2   12
+# 2   6
+# 3   3
+# 1   1
+
+# LOOP I 2, NUMB+1:
+#     NUMB % I == 0:
+#         PRINT(I)
+
+# n = 25
+# i = 5
+# n % i == 0
+#     n // i = 5
+#     n = 5
+
+# n = int(input('Enter a number - '))
+
+# x = 2
+
+# for i in range(2, n + 1):
+#     if n == 1:
+#         print(n)
+#         break
+#     elif n % x == 0:
+#         print(x)
+#         n //= x
+#     else:
+#         x += 1
